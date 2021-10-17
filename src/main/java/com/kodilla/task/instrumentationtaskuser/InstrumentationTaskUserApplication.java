b3b369944bd14056f4efd9a9579e4dceebde48b2
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-17 17:58:22 +0200

    initial commit

---
+++ src/main/java/com/kodilla/task/instrumentationtaskuser/InstrumentationTaskUserApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.instrumentationtaskuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstrumentationTaskUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstrumentationTaskUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/task/instrumentationtaskuser/InstrumentationTaskUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.instrumentationtaskuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstrumentationTaskUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
